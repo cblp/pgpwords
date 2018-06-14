8354ceca3bbe793424e3cc9ffec7d815b974f71b
Yuriy Syrovetskiy <cblp@cblp.su>
2018-06-14 20:57:52 +0300

    Add decoding numbers

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -6,10 +7,10 @@ import           Control.Applicative (optional, (<**>))
 import           Control.Monad (unless)
 import           Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as BSL
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import           Data.Maybe (fromMaybe)
 import           Data.Semigroup ((<>))
-import           Data.Text (Text)
-import qualified Data.Text as Text
-import qualified Data.Text.IO as Text
 import           Data.Word (Word8)
 import           Language.Haskell.TH (listE, runIO, stringE, tupE)
 import           Language.Haskell.TH.Syntax (qAddDependentFile)
@@ -19,7 +20,7 @@ import           Options.Applicative (execParser, flag, fullDesc, helper, info,
                                       strArgument, switch)
 import           Text.Read (readMaybe)
 
-wordlist :: [(Text, Text)]
+wordlist :: [(ByteString, ByteString)]
 wordlist = $(do
     let file = "words.txt"
     qAddDependentFile file
@@ -28,6 +29,11 @@ wordlist = $(do
     unless (length wordPairs == 256) $ fail "bad words file length"
     listE $ tupE . map stringE . words <$> wordPairs
     )
+
+rwordlist :: Map ByteString (Bool, Word8)
+rwordlist = Map.fromList $ do
+    (b, (evenWord, oddWord)) <- zip [0..] wordlist
+    [(evenWord, (False, b)), (oddWord, (True, b))]
 
 data Direction = Encode | Decode
 
@@ -45,9 +51,11 @@ main = do
         Just input -> pure input
         Nothing    -> BSL.getContents
     if numeric then
-        case readMaybe $ BSL.unpack input of
-            Just n  -> Text.putStrLn $ natToWords n
-            Nothing -> fail "input is not a natural number"
+        case direction of
+            Encode -> case readMaybe $ BSL.unpack input of
+                Just n  -> BSL.putStrLn $ natToWords n
+                Nothing -> fail "input is not a natural number"
+            Decode -> print $ wordsToNat input
     else
         fail "binary coding is not implemented yet"
   where
@@ -58,13 +66,20 @@ main = do
     theProgDesc =
         "A tool to encode/decode data and numbers using the PGP word list"
 
-natToWords :: Natural -> Text
-natToWords = Text.unwords . alternate . map byteToWord . natToBytes
+natToWords :: Natural -> ByteString
+natToWords = BSL.unwords . alternate . map byteToWord . natToBytes
   where
     alternate = zipWith ($) $ cycle [fst, snd]
 
-byteToWord :: Word8 -> (Text, Text)
+wordsToNat :: ByteString -> Natural
+wordsToNat = bytesToNat . map wordToByte . BSL.words
+
+byteToWord :: Word8 -> (ByteString, ByteString)
 byteToWord b = wordlist !! fromIntegral b
+
+wordToByte :: ByteString -> Word8
+wordToByte w =
+    snd . fromMaybe (error $ "unknown word " ++ show w) $ Map.lookup w rwordlist
 
 natToBytes :: Natural -> [Word8]
 natToBytes n = fromIntegral m : case d of
@@ -72,3 +87,8 @@ natToBytes n = fromIntegral m : case d of
     _ -> natToBytes d
   where
     (d, m) = n `divMod` 256
+
+bytesToNat :: [Word8] -> Natural
+bytesToNat = \case
+    []     -> 0
+    b : bs -> fromIntegral b + 256 * bytesToNat bs
